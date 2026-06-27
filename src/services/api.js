c8d58ed22c1ea832ec6d7834c7e44bf507f98f96
sravanthi343 <sandelasravanthi343@gmail.com>
2026-06-27 20:04:31 +0530

    Change API base URL to backend service

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://studentcomplaintmanagementsystem-frontend.onrender.com/api',
+  baseURL: 'https://studentcomplaintmanagementsystem-backend.onrender.com',
   timeout: 30000,
 })
 
